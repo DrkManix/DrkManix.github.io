edf4276b7ff37776927415c08569d501bebd6626
DrkManix <45601260+DrkManix@users.noreply.github.com>
2020-07-07 15:23:16 +0200

    horizontalDrag&Scroll + hiddenLoopScroll

--- js/main.js
+++ js/main.js
@@ -1,36 +1,56 @@
+// add function to all info buttons
+$('.info-button').click(function(){
+  // add open class to button
+  $(this).toggleClass("open");
+  // get next element, which is description
+  var description = $(this).next();
+  if ( description.height() ) {
+    // description is open, we want to close it!
+    // so: we delete the max-height attribute
+    description.css('max-height', '0');
+  } else {
+    // description is closed, we want to open it!
+    // scrollHeight is the height of the object, regardless of whether it's displayed or not
+    description.css('max-height', description.prop('scrollHeight') + "px");
+  }
+});
+// add accordion to menu
 $('.menu-button').click(function(){
-   // add open class to button
-   $(this).toggleClass("open");
-   // get next element, which is description
-   var subnav = $(this).next();
-   if ( subnav.height() ) {
-     // description is open, we want to close it!
-     // so: we delete the max-height attribute
-     subnav.css('max-height', '0');
-   } else {
-     // description is closed, we want to open it!
-     // scrollHeight is the height of the object, regardless of whether it's displayed or not
-     subnav.css('max-height', subnav.prop('scrollHeight') + "px");
-   }
- });
+  // add open class to button
+  $(this).toggleClass("open");
+  // get next element, which is description
+  var subnav = $(this).next();
+  if ( subnav.height() ) {
+    // description is open, we want to close it!
+    // so: we delete the max-height attribute
+    subnav.css('max-height', '0');
+  } else {
+    // description is closed, we want to open it!
+    // scrollHeight is the height of the object, regardless of whether it's displayed or not
+    subnav.css('max-height', subnav.prop('scrollHeight') + "px");
+  }
+});
 
- $('document').ready(function() {
 
-     var origDocHeight = document.body.offsetHeight;
 
-     var clone=$(".content").contents().clone();
-     clone.appendTo(".content");
-     clone.prependTo(".content");
+// script for loop scrolling
+//    $('.content').ready(function() {
 
-     $(document).scroll(function(){
+//         var origDocHeight = document.body.offsetHeight;
+//
+//         var clone=$(".content").contents().clone();
+//         clone.appendTo(".content");
+//         clone.prependTo(".content");
 
-         var scrollWindowPos = $(document).scrollTop();
+//         $('.content').scroll(function(){
 
-         if(scrollWindowPos >= origDocHeight ) {
-             $(document).scrollTop(1);
-         }
-         if(scrollWindowPos <= 0 ) {
-              $(document).scrollTop(origDocHeight);
-          }
-     });
- });
+//             var scrollWindowPos = $('.content').scrollTop();
+
+//             if(scrollWindowPos >= origDocHeight ) {
+//                 $('.content').scrollTop(0);
+//             }
+//             if(scrollWindowPos <= 0 ) {
+//                  $('.content').scrollTop(origDocHeight);
+//              }
+//         });
+//     });
